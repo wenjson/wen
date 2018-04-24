09910a3ab560cbd755ecb3fdb95957ace11607e5
Vincent <wenjson@163.com>
2018-04-24 17:02:46 +0800

    First Commit

---
+++ wen/src/main/java/com/vin/WenApplication.java
@@ -0,0 +1,13 @@
+package com.vin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class WenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WenApplication.class, args);
+	}
+}
---
+++ wen/src/test/java/com/vin/wen/WenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vin.wen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
