af2fe4b9b65e664dd7c05d618a760f19b007f813
Vincent <wenjson@163.com>
2018-04-24 18:57:35 +0800

    修改

--- wen/src/main/java/com/vin/WenApplication.java
+++ wen/src/main/java/com/vin/WenApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 /**
  * 应用启动类
  * @author Vincent
- *
+ * @date 2018-04-24
  */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class WenApplication {
