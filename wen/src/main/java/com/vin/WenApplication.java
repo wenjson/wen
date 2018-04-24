65bb5691a1e230f2e8d0aa0154035a35ec606e42
Vincent <wenjson@163.com>
2018-04-24 17:52:26 +0800

    0424 wen

--- wen/src/main/java/com/vin/WenApplication.java
+++ wen/src/main/java/com/vin/WenApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+/**
+ * 应用启动类
+ * @author Vincent
+ *
+ */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class WenApplication {
 
