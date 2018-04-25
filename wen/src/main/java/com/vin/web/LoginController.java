5e932a1001ad016cea794082a344659132bfe39e
Vincent <wenjson@163.com>
2018-04-25 14:43:37 +0800

    0425 wen loginController test

--- wen/src/main/java/com/vin/web/LoginController.java
+++ wen/src/main/java/com/vin/web/LoginController.java
@@ -1,5 +1,6 @@
 package com.vin.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
 	
+	@Value("${file_base_path}")
+	private String filePath;
+	
 	@RequestMapping("/")
 	public String index(){
 		return "Hello World!";
+	}
+	
+	@RequestMapping("/show")
+	public String show(){
+		return filePath;
 	}
 }
